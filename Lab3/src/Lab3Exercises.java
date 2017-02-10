0beee5eb1e6a242920a714f567dcab5e8e0bf123
4015a1 <4015a1@carambola.ece.uprm.edu>
2017-02-10 15:29:40 -0400

    Cambios hechos para contestar pregunta numero 6 del laboratorio 3. Se cambiaron MyPanelClass y Lab3Excercises para lograr crear una ventana que tuviese 2 bordes y un "background" de colores diferentes cada uno.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,9 +3,11 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Gabriel Flores");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);	
 		myFrame.setLocation(800, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(200, 200);		
+		MyPanelClass myPanel = new MyPanelClass();		
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
